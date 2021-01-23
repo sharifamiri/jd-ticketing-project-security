a49129629a0e0ad0305681a8578599229b6c2696
sharifamiri <sharif.amiri4@gmail.com>
2021-01-23 01:07:13 -0800

    Ticketing Project - Security

--- src/main/java/com/cybertek/config/SecurityConfig.java
+++ src/main/java/com/cybertek/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.cybertek.config;
 
+import com.cybertek.service.SecurityService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,11 +11,23 @@ import org.springframework.stereotype.Component;
 @Component
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private SecurityService securityService;
+    private AuthSuccessHandler authSuccessHandler;
+
+    public SecurityConfig(SecurityService securityService, AuthSuccessHandler authSuccessHandler) {
+        this.securityService = securityService;
+        this.authSuccessHandler = authSuccessHandler;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http
                 .authorizeRequests()
+                .antMatchers("/user/**").hasAuthority("Admin")
+                .antMatchers("/project/**").hasAuthority("Manager")
+                .antMatchers("/task/employee/**").hasAuthority("Employee")
+                .antMatchers("/task/**").hasAuthority("Manager")
                 .antMatchers(
                         "/",
                         "/login",
@@ -25,12 +38,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                     .loginPage("/login")
-                    .defaultSuccessUrl("/welcome")
+//                    .defaultSuccessUrl("/welcome")
+                    .successHandler(authSuccessHandler)
                     .failureUrl("/login?error=true")
                     .permitAll()
                 .and()
                 .logout()
                     .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                    .logoutSuccessUrl("/login");
+                    .logoutSuccessUrl("/login")
+                .and()
+                .rememberMe()
+                    .tokenValiditySeconds(120)
+                    .key("cybertekSecret")
+                    .userDetailsService(securityService);
     }
 }
